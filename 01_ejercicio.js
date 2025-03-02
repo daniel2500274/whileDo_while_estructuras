e4ebdaeb93facda066bd496e732889a88ff55a65
Daniel Universidad <edramirezd@alumno.uspg.edu.gt>
2025-03-01 21:42:44 -0600

    validaciones area de un triangulo y suma de dos numeros

--- 01_ejercicio.js
+++ 01_ejercicio.js
@@ -27,6 +27,29 @@ do {
             alert("El promedio es: " + promedio);
             break;
 
+        case 2: // Área de un rectángulo
+            let base = parseFloat(prompt("Ingrese la base del rectángulo:"));
+            let altura = parseFloat(prompt("Ingrese la altura del rectángulo:"));
+            if (isNaN(base) || isNaN(altura) || base < 0 || altura < 0) {
+                alert("Datos ingresados no validos");
+                break;
+            }
+            let area = base * altura;
+            alert("El área del rectángulo es: " + area);
+            break;
+
+        case 3: // Suma de dos números
+            let num1 = parseFloat(prompt("Ingrese el primer número:"));
+            let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+            if (isNaN(num1) || isNaN(num2)) {
+                alert("Datos ingresados no validos");
+                break;
+            }
+            let suma = num1 + num2;
+            alert("La suma es: " + suma);
+            break;
+
+
         case 5:
             alert("Saliendo del programa...");
             break;
